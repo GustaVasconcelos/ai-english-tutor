3874158c412f09d1dc29e57a68a4d1b5ba05f9f2
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2026-02-27 14:16:27 -0300

    feat: adiciona configuracao inicial para laravel ai sdk

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Ai\Agents\EnglishTutor;
 
 Route::get('/ping', function () {
     return response()->json([
         'pong' => 'ok'
     ]);
+});
+
+Route::get('/ai-test', function () {
+    $response = EnglishTutor::make()->prompt('Explique o verbo "to be"');
+
+    return (string)$response;
 });
